622804f062965793aff39f155ee1bb13f216a590
泡泡 <qinjia@outlook.com>
2025-07-29 19:23:59 +0800

    feat: update types

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,16 @@ import { MonacoAutoTypings, Monaco, MonacoEditor } from './core/auto-typings';
 import { IAutoTypingsOptions } from './types/index';
 
 /**
- * 初始化自动类型提示功能
- * @param monaco Monaco编辑器实例
- * @param editor 编辑器实例
- * @param options 配置选项
+ * Initialize auto typings
+ * @param monaco monaco 
+ * @param editor monaco editor instance
+ * @param options Configuration options
  */
 export default function initialize(
   monaco: Monaco,
   editor: MonacoEditor,
   options: IAutoTypingsOptions = {}
-): Promise<{ dispose: () => void }> {
+): { dispose: () => void } {
   const autoTypings = new MonacoAutoTypings(options);
   return autoTypings.initialize(monaco, editor);
 }
